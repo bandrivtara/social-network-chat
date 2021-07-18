b7dc7baada7c239462ec63761687de3d0b355cd7
bandrivtara <taras.bandrivskyy@duolife.eu>
2021-07-18 17:54:22 +0200

    SNC-1 create Login files

---
+++ src/modules/auth/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Login:React.FC<PropsTypes> = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+type PropsTypes = {}
---
+++ src/modules/auth/Login/LoginContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Login} from "./Login";
+
+export const LoginContainer:React.FC<PropsTypes> = () => {
+
+    return (
+        <Login/>
+    );
+};
+
+type PropsTypes = {}
