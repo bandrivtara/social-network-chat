042956c5e3b93575c42d8be5dc1c3ec24892fba8
taras.bandrivskyy <taras.bandrivskyy@duolife.eu>
2021-07-20 18:09:10 +0200

    SNC-3 install redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
 import {TBIntlProvider} from "./intl/TBIntlProvider";
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
     <React.StrictMode>
+
         <TBIntlProvider>
             <App/>
         </TBIntlProvider>
