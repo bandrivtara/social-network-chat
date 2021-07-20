d52134f04679902222b4ac679ef1b5105c4177fb
bandrivtara <bandrivtara@gmail.com>
2021-07-20 18:27:58 +0200

    SNC-3 check

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
 import {TBIntlProvider} from "./intl/TBIntlProvider";
-import { Provider } from 'react-redux'
 
 ReactDOM.render(
     <React.StrictMode>
